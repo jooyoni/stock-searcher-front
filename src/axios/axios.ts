4aa27171c62928fce0081a50ffd1b3483bdef583
이주연 <jooyeon@ijuyeon-ui-MacBookAir.local>
2025-01-03 17:29:41 +0900

    refactor: baseURL fixed

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  // baseURL: `https://neat-salmon-jooyeon-c3c36dcb.koyeb.app/`,
-  baseURL: `http://localhost:3001`,
+  baseURL: `https://neat-salmon-jooyeon-c3c36dcb.koyeb.app/`,
 });
